1ff054d493621769e0eb31c1d54fd2f16bbc8ec2
DanGdl <dangladste@gmail.com>
2024-03-25 15:58:48 +0200

    spi2eth frequency update

--- psu_init.c
+++ psu_init.c
@@ -1191,18 +1191,18 @@ unsigned long psu_clock_init_data(void)
     *  PSU_CRL_APB_SPI1_REF_CTRL_DIVISOR1                          0x1
 
     * 6 bit divider
-    *  PSU_CRL_APB_SPI1_REF_CTRL_DIVISOR0                          0x5
+    *  PSU_CRL_APB_SPI1_REF_CTRL_DIVISOR0                          0x13
 
     * 000 = IOPLL; 010 = RPLL; 011 = DPLL; (This signal may only be toggled af
     * ter 4 cycles of the old clock and 4 cycles of the new clock. This is not
     *  usually an issue, but designers must be aware.)
-    *  PSU_CRL_APB_SPI1_REF_CTRL_SRCSEL                            0x2
+    *  PSU_CRL_APB_SPI1_REF_CTRL_SRCSEL                            0x0
 
     * This register controls this reference clock
-    * (OFFSET, MASK, VALUE)      (0XFF5E0080, 0x013F3F07U ,0x01010502U)
+    * (OFFSET, MASK, VALUE)      (0XFF5E0080, 0x013F3F07U ,0x01011300U)
     */
 	PSU_Mask_Write(CRL_APB_SPI1_REF_CTRL_OFFSET,
-		0x013F3F07U, 0x01010502U);
+		0x013F3F07U, 0x01011300U);
 /*##################################################################### */
 
     /*
